b42666a527ab08745310a927d5f14292efa6453f
Rutwik Shinde <rutwikshinde34@gmail.com>
2024-03-03 22:02:59 +0530

    changed the sequence of reviews displayed

--- src/components/CustomerReviews.jsx
+++ src/components/CustomerReviews.jsx
@@ -94,19 +94,19 @@ const CustomerReviews = () => {
       </div> */}
 
       <p className="mb-4 xl:text-4xl text-3xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-yellow-200 to-yellow-400 transition-all ease-in-out duration-100">
-        Review - Shanipar Branch
+        Review - Karve Road Branch
       </p>
       <div
-        class="elfsight-app-1bd76106-5952-4328-9a28-4cd829587ab6"
+        class="elfsight-app-c9ce9c46-f9cc-498a-afb0-53659c6448ec"
         data-elfsight-app-lazy
       ></div>
       <div className="w-[100vw] relative bg-primary z-10 h-10 mt-[-3rem]"></div>
 
       <p className="mb-4 xl:text-4xl text-3xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-yellow-200 to-yellow-400 transition-all ease-in-out duration-100">
-        Review - Karve Road Branch
+        Review - Shanipar Branch
       </p>
       <div
-        class="elfsight-app-c9ce9c46-f9cc-498a-afb0-53659c6448ec"
+        class="elfsight-app-1bd76106-5952-4328-9a28-4cd829587ab6"
         data-elfsight-app-lazy
       ></div>
       <div className="w-[100vw] relative bg-primary z-10 h-10 mt-[-3rem]"></div>
